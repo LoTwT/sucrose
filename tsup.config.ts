78c916b1b9900212c5e7d66da464f957ba9686c1
LoTwT <709937065@qq.com>
2023-08-18 10:55:43 +0800

    chore: update tsup config banner

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
   clean: true,
   target: "node16",
   dts: true,
-  shims: true,
+  banner: ({ format }) => {
+    if (format === "esm") {
+      return {
+        js: `const require = (await import("node:module")).createRequire(import.meta.url);
+const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+const __dirname = (await import("node:path")).dirname(__filename);`,
+      }
+    }
+  },
 })
