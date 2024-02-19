274074d664d594dd7f2e36463d4641938a8c2a4b
LoTwT <709937065@qq.com>
2024-02-19 14:56:28 +0800

    feat: unify types name

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,8 @@
 export type Nullable<T> = T | null
+export type MayBeNull<T> = T | null
 
+export type Undefinedable<T> = T | undefined
 export type MayBeUndefined<T> = T | undefined
 
 export type Arrayable<T> = T | T[]
+export type MayBeArray<T> = T | T[]
